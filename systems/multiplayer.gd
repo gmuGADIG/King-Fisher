f2f82e9e57e4fdaf3b234d789565ea4063ec4848
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2026-01-18 14:51:55 -0500

    rpc

---
+++ systems/multiplayer.gd
@@ -0,0 +1,43 @@
+extends Node
+
+signal new_player(id: int)
+
+const PORT = 25575
+const MAX_CLIENTS = 3
+
+var player_list: Array[int] = []
+
+func _on_peer_connected(id: int) -> void:
+	player_list.push_back(id)
+	new_player.emit(id)
+	
+	if multiplayer.is_server():
+		learn_players.rpc_id(id, player_list)
+
+
+@rpc("reliable")
+func learn_players(new_player_list: Array[int]) -> void:
+	for player in new_player_list:
+		if not player in player_list:
+			player_list.push_back(player)
+			new_player.emit(player)
+
+
+func _ready() -> void:
+	pass
+
+func create_server() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(PORT, MAX_CLIENTS)
+	multiplayer.multiplayer_peer = peer
+	
+
+func join_server(ip : String) -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip, PORT)
+	multiplayer.multiplayer_peer = peer
+	
+
+@rpc
+func print_hello() -> void:
+	print("hello")
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -2,13 +2,13 @@ extends Control
 
 
 func _on_host_button_pressed() -> void:
-	MultiplayerBus.create_server()
+	Multiplayer.create_server()
 	print("Host Created")
 	get_tree().change_scene_to_file("res://temp/temp_lobby/temp_lobby.tscn")
 
 
 
 func _on_connect_button_pressed() -> void:
-	MultiplayerBus.join_server($VBoxContainer/JoinGame/IP.text,$VBoxContainer/JoinGame/Port.text)
+	Multiplayer.join_server($VBoxContainer/JoinGame/IP.text)
 	print("Lobby Joined")
 	get_tree().change_scene_to_file("res://temp/temp_lobby/temp_lobby.tscn")
