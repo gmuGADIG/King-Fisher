6203ba8ba04ff302b67aed7f060681e47efac0d4
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2026-01-18 15:38:51 -0500

    P

--- player.gd
+++ player.gd
@@ -1,10 +1,7 @@
 class_name Player
 extends CharacterBody2D
 
-@export var punch_direction := 1.
 @export var speed := 500.
-var can_punch := true
-var punched := false
 
 func _ready() -> void:
 	%BoxingGlove.hide()
@@ -24,41 +21,12 @@ func _process(_delta: float) -> void:
 		move_and_slide()
 		return
 	
-	if Input.is_action_just_pressed("punch"): punch.rpc()
-	if not punched:
-		var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-		velocity = input * speed
+	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input * speed
 	
 	sync_velocity.rpc(velocity)
 	move_and_slide()
 
-@rpc("call_local", "reliable")
-func punch() -> void:
-	if not can_punch: return
-	
-	can_punch = false
-	
-	%BoxingGlove.show()
-	await create_tween().tween_property(%BoxingGlove, "position:x", 83 * sign(punch_direction), .05).finished
-	await create_tween().tween_property(%BoxingGlove, "position:x", 0, .75).finished
-	%BoxingGlove.hide()
-	
-	can_punch = true
-
 @rpc
 func sync_velocity(vel: Vector2) -> void:
 	velocity = vel
-
-func _on_boxing_glove_body_entered(body: Node2D) -> void:
-	if body == self: return
-	if body is Player:
-		var player := body as Player
-		player.punched = true
-		var dir := global_position.direction_to(player.global_position)
-		dir = quantize_direction(dir, 4)
-		player.velocity = dir * 2000
-		
-		
-		await get_tree().create_timer(.25).timeout
-		
-		player.punched = false
--- temp/temp_lobby/temp_lobby.gd
+++ temp/temp_lobby/temp_lobby.gd
@@ -2,20 +2,25 @@ extends Node2D
 
 func _ready() -> void:
 	Multiplayer.new_player.connect(on_player_join)
-	if multiplayer.is_server():
-		spawn_player.rpc(1, Color.WHITE, Vector2(150, randf_range(100, 500)), 1)
+	spawn_player.rpc(1, Color.WHITE, Vector2(150, randf_range(100, 500)))
 		#spawn_player.rpc(Multiplayer.player_list[0], Color.YELLOW, Vector2(800, randf_range(100, 500)), -1)
 
 func on_player_join(id : int) -> void:
-	spawn_player(id, Color.YELLOW, Vector2(150, randf_range(100, 500)), -1)
+	print_players()
+	spawn_player(id, Color(1,0,0,0.5), Vector2(150, randf_range(100, 500)))
 	pass
 
 @rpc("reliable", "call_local")
-func spawn_player(id: int, color: Color, pos: Vector2, punch_dir: float) -> void:
+func spawn_player(id: int, color: Color, pos: Vector2) -> void:
 	var player: Player = preload("res://player.tscn").instantiate()
 	player.set_multiplayer_authority(id)
 	player.modulate = color
 	player.position = pos
-	player.punch_direction = punch_dir
 	
 	add_child(player)
+
+func print_players() -> void:
+	if multiplayer.is_server():
+		print("players:")
+		for player_id in Multiplayer.player_list:
+			print(player_id)
