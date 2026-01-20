faf95eafc5ba9a94c12e25fde353c9fb19f0fe05
KaratePikachu <80354180+KaratePikachu@users.noreply.github.com>
2026-01-19 20:34:07 -0500

    Made lobby (extends world base)

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -7,11 +7,11 @@ func _on_host_button_pressed() -> void:
 		#name_text.text = "Player"+str(randi_range(1,999))
 	Multiplayer.create_server()
 	Debug.log("Host Created")
-	get_tree().change_scene_to_file("res://world/world_base.tscn")
+	get_tree().change_scene_to_file("res://world/lobby/lobby.tscn")
 
 
 
 func _on_connect_button_pressed() -> void:
 	Multiplayer.join_server($VBoxContainer/JoinGame/IP.text)
 	Debug.log("Lobby Joined")
-	get_tree().change_scene_to_file("res://world/world_base.tscn")
+	get_tree().change_scene_to_file("res://world/lobby/lobby.tscn")
