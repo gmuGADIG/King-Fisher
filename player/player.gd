1b54ac53d092513f9b37ef690f89b675a1a960aa
trinity <tahimes4000@gmail.com>
2026-02-11 20:56:23 -0500

    WIP Gravity and Jumping
    Started adding gravity and falling acceleration and beginning to try a different way of player movement.

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 
 @export var speed := 10.
 
+# The downward acceleration when in the air
+@export var fall_acceleration := 75
+
 var last_pos : Vector3 = Vector3.ZERO
 
 func _process(_delta: float) -> void:
@@ -14,6 +17,12 @@ func _process(_delta: float) -> void:
 	velocity = Vector3(input.x,0,input.y) * speed
 	
 	sync_velocity.rpc(velocity)
+	move_and_slide()
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		last_pos.y = last_pos.y - (fall_acceleration * delta)
+	
 	move_and_slide()
 
 func _input(event: InputEvent) -> void:
