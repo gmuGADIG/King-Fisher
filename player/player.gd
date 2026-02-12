8a60fb9be8c607294a77506d799e5aec46f8cc12
TheCheeseler <115675068+PekkaBoi38@users.noreply.github.com>
2026-02-11 21:21:08 -0500

    Added pseudocode logic and framework for players
    Added pseudocode logic to highlight how player readying will be implemented. Made clients have a boolean to signify whenever they are ready or not, while hosts have pseudocode on how to interpret client responses

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 signal client_space(id: int)
 
 @export var speed := 10.
+@export var clientready := false
 
 var last_pos : Vector3 = Vector3.ZERO
 
@@ -21,7 +22,21 @@ func _process(_delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("print_players"):
 		Debug.print_players()
+	if event.is_action_pressed("jump") and not is_multiplayer_authority():
+		print("I space and client")
+		clientready = not clientready
+		client_space.emit(0, clientready)
+	if event.is_action_pressed("jump") and is_multiplayer_authority():
+		print("I space and host")
+		var list = get_tree().root.get_child(0).player_list
+		
+		#if(all clients ready (n - 1 players, where n is players in lobby):
+		#	print("Everyone is ready, starting...")
+		#else:
+		#	print("not everyone is ready, wait")
+		
 
 @rpc("unreliable_ordered")
 func sync_velocity(vel: Vector3) -> void:
 	velocity = vel
+	
