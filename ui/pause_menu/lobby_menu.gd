574df8ca0ab8506a2ad214a1a14c1387a52eae10
Clancy M <Cmanatron@gmail.com>
2026-02-18 21:28:17 -0500

    LobbyMenuScript
    Lobby Menu has a script now

---
+++ ui/pause_menu/lobby_menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+@export var roundTimer:int 
+@export var itemSpawn:int
+@export var fishSpawn:int
+@export var itemSelect:String
+@export var mapSelect:String
+@export var musicSelect:String
