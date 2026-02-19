d13b233d000d9eecf54302e92d1fb7bdeecd1ea8
Dev Ajit <dajit8161@gmail.com>
2026-02-18 21:05:50 -0500

    Basic menu elements
    Added some code for basic menu elements (sliders) and signals for letting the elements communicate with each other. The menu isn't showing though

---
+++ ui/options/options_menu.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+@onready var audio
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Options"):
+		print("Options Menu!")
+		visible = not visible
+
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
