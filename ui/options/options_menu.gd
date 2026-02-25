2a045ec92bef50212ca74304be4627d95654c5ce
KaratePikachu <KaidenKeyZ@gmail.com>
2026-02-25 16:54:22 -0500

    cleanup

--- ui/options/options_menu.gd
+++ ui/options/options_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var audio
+@onready var audo_player : AudioStreamPlayer = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,9 +14,15 @@ func _process(delta: float) -> void:
 
 func _on_master_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value / 30)
+	audo_player.bus = "Master"
+	audo_player.play()
 
 func _on_sfx_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value / 30)
+	audo_player.bus = "SFX"
+	audo_player.play()
 
 func _on_music_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value / 30)
+	audo_player.bus = "Music"
+	audo_player.play()
