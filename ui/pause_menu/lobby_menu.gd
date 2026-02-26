05e58296a6584f2135efe7656381dae739b733b9
Pikafan16543 <bruinsstar17@gmail.com>
2026-02-25 20:18:47 -0500

    settings saved to variables
    saves current settings to variables upon hitting the save button

--- ui/pause_menu/lobby_menu.gd
+++ ui/pause_menu/lobby_menu.gd
@@ -6,3 +6,31 @@ extends Control
 @export var itemSelect:String
 @export var mapSelect:String
 @export var musicSelect:String
+
+@onready var timerSpinBox:SpinBox = $"CanvasLayer/Panel/VBoxContainer/Timer SpinBox"
+@onready var itemSpinBox:SpinBox = $"CanvasLayer/Panel/VBoxContainer/Item Spawn SpinBox"
+@onready var fishSpinBox:SpinBox = $"CanvasLayer/Panel/VBoxContainer/Fish Spawn SpinBox"
+@onready var itemButton:OptionButton = $"CanvasLayer/Panel/VBoxContainer/Item OptionButton"
+@onready var mapButton:OptionButton = $"CanvasLayer/Panel/VBoxContainer/Map OptionButton"
+@onready var musicButton:OptionButton = $"CanvasLayer/Panel/VBoxContainer/Music OptionButton"
+
+
+
+
+
+func _on_save_button_pressed() -> void:
+	roundTimer = int(timerSpinBox.value)
+	itemSpawn = int(itemSpinBox.value)
+	fishSpawn = int(fishSpinBox.value)
+	itemSelect = itemButton.get_item_text(itemButton.get_selected())
+	mapSelect = mapButton.get_item_text(mapButton.get_selected())
+	musicSelect = musicButton.get_item_text(musicButton.get_selected())
+	print(roundTimer)
+	print(itemSpawn)
+	print(fishSpawn)
+	print(itemSelect)
+	print(mapSelect)
+	print(musicSelect)
+	
+	
+	
