c4742dd056304c241fd674f00818ab8ae0bec078
Kutsa <kutsa2004@gmail.com>
2026-03-15 12:22:32 -0400

    Added todo coomment

--- items/item_helmet/item_helmet.gd
+++ items/item_helmet/item_helmet.gd
@@ -3,5 +3,6 @@ extends Item
 func use() -> void:
 	if !is_held: Debug.log_err("Item was used while not held, WTF?")
 	else:
+		# Todo: Impliment animation once we have them.
 		get_parent().items.helmet = true
 		queue_free()
