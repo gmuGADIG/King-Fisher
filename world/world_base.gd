6c65361966301421277c661b3651dec390a1446f
Michael Campbell <me@micycle.dev>
2026-03-17 23:32:16 -0400

    refine spawning a bit more

--- world/world_base.gd
+++ world/world_base.gd
@@ -13,21 +13,23 @@ func _ready() -> void:
 		$CanvasLayer/VBoxContainer/Label.text = "Client"
 
 func on_player_join(id : int) -> void:
+	# this function only runs on the server
 	if not multiplayer.is_server():
 		return
 	
+	for child in get_children():
+		if child is Player:
+			var p: Player = child
+			spawn_player.rpc_id(id, p.get_multiplayer_authority(), p.position)
 	
-	for player_id in Multiplayer.player_list:
-		if player_id != id:
-			spawn_player.rpc_id(id,player_id,Vector3.ZERO)
-	spawn_player.rpc(id,Vector3.ZERO)
+	spawn_player.rpc(id,$Players.get_safe_spawn_point())
 	
 
 @rpc("reliable", "call_local")
 func spawn_player(id: int, pos: Vector3) -> void:
 	Debug.log("Creating player of id ",id)
 	var new_player: Player = player.instantiate()
-	new_player.set_multiplayer_authority(id)
 	
-	add_child(new_player)
 	new_player.position = pos
+	add_child(new_player)
+	new_player.set_authority(id) # we don't need to use RPC here since this function call is RPC'd
