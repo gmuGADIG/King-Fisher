a11705cfd9f28dbea787c92ad4a02cc22460f7b1
KaratePikachu <KaidenKeyZ@gmail.com>
2026-03-25 18:33:11 -0400

    Change disconnect to main menu

--- systems/multiplayer.gd
+++ systems/multiplayer.gd
@@ -136,7 +136,7 @@ func disconnect_client(msg : String) -> void:
 	multiplayer.multiplayer_peer.disconnect_peer(multiplayer.get_unique_id())
 	#player_list.erase(id)
 	#if my_id == id:
-	get_tree().change_scene_to_file("res://ui/lobby_browser/lobby_browser.tscn")
+	get_tree().change_scene_to_file("res://ui/main_menu/main_menu.tscn")
 	return
 #
 ## client displaying error message when disconnected
