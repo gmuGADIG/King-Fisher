60c13ee98d5e1a42e7cfedd1eb76d66170121990
Logan-Omrri <loganwdowns05@gmail.com>
2026-04-01 21:55:13 -0400

    Worked on the scoreboard
    i dunno man

---
+++ ui/scoreboard/scoreboard.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var playerRowScene = load("res://ui/scoreboard/player_row.gd")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# Call whenever a player joins or leaves the server.
+func _updateScoreboard() -> void:
+	# for i in players:
+		# var playerRowInstance = playerRowScene.instantiate()
+		# add playerRowInstance as a child of Scoreboards first child
+	pass
