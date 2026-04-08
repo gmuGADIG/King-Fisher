04a68df752ffbd4ad9d9dca3afd96044ead48c99
Kutsa <kutsa2004@gmail.com>
2026-04-08 01:14:18 -0400

    Fish Done

--- ui/livewell/livewell.gd
+++ ui/livewell/livewell.gd
@@ -3,18 +3,22 @@ extends Control
 @export var fish_info_packed : PackedScene
 
 var fish_inventory: Dictionary[String, livewell_fish_info] = {}
-var current_fish : Fish
 @onready var livewellPanel : Panel = $Background
 @onready var score : Label = $Background/Score
-@onready var top_fish_container : VBoxContainer = $Background/TopFishPanel/TopFishContainer
-var top_fish : Array = []
+@onready var top_fish_container : HBoxContainer = $Background/TopFishPanel/TopFishContainer
+@onready var bottom_fish_container : HBoxContainer = $Background/BottomFishPanel/BottomFishContainer
+var top_fish : Dictionary[String, LivewellFish] = {}
+var bottom_fish : Dictionary[String, LivewellFish] = {}
 var current_score : int = 0;
 
 func _ready() -> void:
+	var item_size = 175
+	var offset_distance = item_size + (item_size * 0.15)
+
+	top_fish_container.add_theme_constant_override("separation", offset_distance)
 	hide()
 
 func addFish(new_fish : Fish, amount : int = 1) -> void:
-	current_fish = new_fish
 	if fish_inventory.has(new_fish.fish_name):
 		fish_inventory.get(new_fish.fish_name).fish_count += amount
 	else:
@@ -26,6 +30,16 @@ func addFish(new_fish : Fish, amount : int = 1) -> void:
 	changeScore(new_fish.get_score())
 	updateVisual()
 
+func removeFish(fish : Fish, amount : int = 1) -> void:
+	print(fish_inventory.size())
+	if(!fish_inventory.has(fish.fish_name) or fish_inventory.size() == 0): return
+	elif fish_inventory.get(fish.fish_name).fish_count <= amount:
+		fish_inventory.get(fish.fish_name).fish_count = 0
+	else:
+		fish_inventory.get(fish.fish_name).fish_count -= amount
+	changeScore(-fish.get_score())
+	updateVisual()
+
 func changeScore(change : int):
 	current_score += change
 	# Failsafe to prevent negative score
@@ -33,39 +47,48 @@ func changeScore(change : int):
 		current_score = 0
 	score.text = str(current_score) + " pts"
 
-func removeFish(fish : Fish, amount : int = 0) -> void:
-	if(fish_inventory.size() == 0 or !fish_inventory.has(fish.fish_name)):
-		return
-	if fish_inventory[fish.fish_name].fish_count >= amount:
-		fish_inventory[fish.fish_name].fish_count = 0
-	else:
-		fish_inventory[fish.fish_name].fish_count -= amount
-	changeScore(-fish.get_score())
-	updateVisual()
-
 func updateVisual() -> void:
 	for fish in fish_inventory.values():
-		var fish_info : LivewellFish = fish_info_packed.instantiate()
-		if top_fish.has(fish.fish_name()):
-			fish_info = top_fish.get(top_fish.find(fish.name))
-		fish_info.name = fish.fish_name()
-		# fish_info.set_fish("res://fish/"+fish.fish.get_grade_string().to_lower()+"/"+fish.fish_name()+".tres", fish)
-		fish_info.set_fish(fish.fish.sprite.get_path(), fish)
-		top_fish_container.add_child(fish_info)
-		if not top_fish.has(fish.name):
-			top_fish.append(fish_info)
+		var fish_name = fish.fish_name()
+		if top_fish.has(fish_name):
+			var fish_info : LivewellFish = top_fish.get(fish_name)
+			if fish.fish_count == 0:
+				fish_info.queue_free()
+				top_fish.erase(fish_name)
+				fish_inventory.erase(fish_name)
+			elif fish.fish_count > 1:
+				fish_info.count.text = str(fish.fish_count) + "x"
+				fish_info.count.visible = true
+		elif bottom_fish.has(fish_name):
+			var fish_info : LivewellFish = bottom_fish.get(fish_name)
+			if fish.fish_count == 0:
+				fish_info.queue_free()
+				bottom_fish.erase(fish_name)
+				fish_inventory.erase(fish_name)
+			else:
+				fish_info.count.text = str(fish.fish_count) + "x"
+				fish_info.count.visible = true
+		else:
+			var fish_info : LivewellFish = fish_info_packed.instantiate()
+			fish_info.name = fish_name
+			fish_info.set_fish(fish.fish.sprite.get_path(), fish)
+			if top_fish_container.get_child_count() < 5:
+				top_fish_container.add_child(fish_info)
+				top_fish.set(fish_name, fish_info)
+			elif bottom_fish_container.get_child_count() < 5:
+				bottom_fish_container.add_child(fish_info)
+				bottom_fish.set(fish_name, fish_info)
 
-		
-	for fish in fish_inventory.values():
-		print("Fish: " + fish.fish_name() + " Count: " + str(fish.fish_count) + " Grade: " + fish.fish.get_grade_string())
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("livewell_menu"):
 		visible = !visible
 	if event.is_action_pressed("add_fish"):
-		var newFish : Fish = load("res://fish/sushi/test_fish.tres")
+		var newFish : Fish = load("res://fish/fresh/test_fish.tres")
 		var newFish1 : Fish = load("res://fish/sushi/fish_seven.tres")
 		addFish(newFish)
-		addFish(newFish1)
+		# addFish(newFish1)
 	if event.is_action_pressed("remove_fish"):
-		var newFish : Fish = load("res://fish/sushi/test_fish.tres")
+		var newFish : Fish = load("res://fish/fresh/test_fish.tres")
+		var newFish1 : Fish = load("res://fish/sushi/fish_seven.tres")
 		removeFish(newFish)
+		# removeFish(newFish1)
--- ui/livewell/livewell_fish.gd
+++ ui/livewell/livewell_fish.gd
@@ -6,10 +6,24 @@ var fish_info : livewell_fish_info
 
 @onready var background : Panel = $Background
 @onready var fish_sprite : TextureRect = $Background/FishSprite
+@onready var points_label : Label = $Points
+@onready var underline_points : ColorRect = $PointsUnderline
+@onready var count : Label = $Count
+
+func _ready() -> void:
+	count.visible = false
 
 func set_fish(path : String, info : livewell_fish_info) -> void:
 	self.fish_file_path = path
 	self.fish_info = info
+	load_fish.call_deferred()
+	
+func load_fish() -> void:
+	fish_sprite.texture = load(fish_file_path)
+	background.get_theme_stylebox("panel").set_bg_color(get_fish_color())
+	points_label.text = str(fish_info.fish.get_score()) + " pts"
+	underline_points.size.x = get_text_pixel_width(points_label.text, points_label) * 1.25
+	underline_points.position.x = points_label.position.x + (points_label.size.x / 2) - (underline_points.size.x / 2)
 
 func get_fish_color() -> Color:
 	match fish_info.fish.grade:
@@ -24,8 +38,9 @@ func get_fish_color() -> Color:
 		_:
 			return Color.WHITE # White for Unset or unknown grades
 
-func _process(_delta: float) -> void:
-	if fish_info != null:
-		fish_sprite.texture = load(fish_file_path)
-		background.get_theme_stylebox("panel").set_bg_color(get_fish_color())
-	return
+func get_text_pixel_width(text: String, label_node: Label) -> float:
+	var font = label_node.get_theme_font("font")
+	var font_size = label_node.get_theme_font_size("font_size")
+
+	var size = font.get_string_size(text, HORIZONTAL_ALIGNMENT_CENTER, -1, font_size)
+	return size.x
