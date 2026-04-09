dd3d3900662bdaff09751a92adeecc233f7b9267
Camber Boles <camberb51@gmail.com>
2026-04-08 21:34:05 -0400

    Round timer 'spin box', replace lobby menu in lobby scene

--- ui/lobby_settings/lobby_menu.gd
+++ ui/lobby_settings/lobby_menu.gd
@@ -7,6 +7,9 @@ enum SpawnRate {
 	HIGH
 }
 
+## The maximum time allowed for a round, in seconds.
+@export var maximumRoundTime: int = 300
+
 @export_group("Default Settings")
 
 # TODO: replace with names of actual maps
@@ -15,9 +18,9 @@ enum SpawnRate {
 var defaultMapSelect: int
 
 ## The default song selection for the round.
-@export var defaultMusicSelect: String
+@export var defaultMusicSelect: int
 ## The amount of time for each round by default, in seconds.
-@export var defaultRoundTimer: int = 60
+@export var defaultRoundTime: int = 60
 ## The default spawn rate for items.
 @export var defaultItemSpawn: SpawnRate = SpawnRate.MEDIUM
 ## The default spawn rate for fish.
@@ -28,12 +31,18 @@ var defaultMapSelect: int
 	"Glue Grenade", "Helmet", "Ziplock Bag", "Golden Worm")
 var defaultItemSelect: int
 
-static var roundTimer: int
+## The current length of each round, in seconds.
+static var roundTime: int
+## The current spawn rate of items (Low, Medium, or High).
 static var itemSpawn: SpawnRate
+## The current spawn rate of fish (Low, Medium, or High).
 static var fishSpawn: SpawnRate
+## The currently selected items to spawn in this game.
 static var itemSelect: int
+## The currently selected maps to use in this game.
 static var mapSelect: int
-static var musicSelect: String
+## The ID of the currently selected song.
+static var musicSelect: int
 
 @onready var panel: Control = $Background
 @onready var timerInput: LineEdit = %MinuteBox
@@ -44,22 +53,31 @@ static var musicSelect: String
 @onready var musicButton: OptionButton = %SongOptions
 
 func _ready() -> void:
+	_on_reset_button_pressed()
 	panel.hide()
 	panel.draw.connect(update_menu);
 
+## Refreshes the menu with the current settings.
+## This is called automatically when the panel becomes visible.
 func update_menu() -> void:
-	pass
+	update_timer()
+	itemRateSelection.select(itemSpawn)
+	fishRateSelection.select(fishSpawn)
+	set_buttons_from_bitmask(itemSelectList, itemSelect)
+	set_buttons_from_bitmask(mapSelectList, mapSelect)
+	musicButton.select(musicButton.get_item_index(musicSelect))
 
+## Save current lobby settings to memory.
 func _on_save_button_pressed() -> void:
 	# Round timer
 	var timerText := timerInput.text.split(":")
 	match timerText.size():
 		2:
-			roundTimer = int(timerText[0]) * 60 + int(timerText[1])
+			roundTime = int(timerText[0]) * 60 + int(timerText[1])
 		1:
-			roundTimer = int(timerText[0]) * 60
+			roundTime = int(timerText[0]) * 60
 		_:
-			roundTimer = defaultRoundTimer
+			roundTime = defaultRoundTime
 	# Spawn rates
 	if itemRateSelection.is_anything_selected():
 		itemSpawn = itemRateSelection.get_selected_items()[0] as SpawnRate
@@ -70,27 +88,28 @@ func _on_save_button_pressed() -> void:
 	# Selections
 	itemSelect = get_bitmask_from_buttons(itemSelectList)
 	mapSelect = get_bitmask_from_buttons(mapSelectList)
-	musicSelect = musicButton.get_item_text(musicButton.get_selected())
-	print(roundTimer)
+	musicSelect = musicButton.get_item_id(musicButton.get_selected())
+	print(roundTime)
 	print(itemSpawn)
 	print(fishSpawn)
 	print(itemSelect)
 	print(mapSelect)
 	print(musicSelect)
-	# File write
-	var host_save = FileAccess.open("user://host_settings.cfg", FileAccess.WRITE)
-	host_save.store_string(JSON.stringify({
-		"roundTimer": roundTimer,
-		"itemSpawn": itemSpawn,
-		"fishSpawn": fishSpawn,
-		"itemSelect": itemSelect,
-		"mapSelect": mapSelect,
-		"musicSelect": musicSelect
-	}))
 
+## Reset settings to default.
+func _on_reset_button_pressed() -> void:
+	roundTime = defaultRoundTime
+	itemSpawn = defaultItemSpawn
+	fishSpawn = defaultFishSpawn
+	itemSelect = defaultItemSelect
+	mapSelect = defaultMapSelect
+	musicSelect = defaultMusicSelect
+	update_menu()
+
+## Stores the set of selected buttons as a bitmask.
 func get_bitmask_from_buttons(buttons: Array[Node]) -> int:
 	var mask: int = 0
-	var inc := 0b1
+	var inc: int = 1
 	for button in buttons:
 		if button is BaseButton:
 			if button.button_pressed:
@@ -98,8 +117,46 @@ func get_bitmask_from_buttons(buttons: Array[Node]) -> int:
 			inc <<= 1
 	return mask
 
+## Uses a bitmask to set the state of a list of buttons.
+func set_buttons_from_bitmask(buttons: Array[Node], bitmask: int) -> void:
+	for button in buttons:
+		if button is BaseButton:
+			button.button_pressed = bool(bitmask & 1)
+			bitmask >>= 1
+
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("Menu") && multiplayer.is_server()):
 		print("Menu pressed")
 		print("Authority checked")
 		panel.visible = !panel.visible
+
+func _on_timer_more_button_pressed() -> void:
+	if (roundTime < maximumRoundTime):
+		roundTime = clampi(roundTime + 30, 0, maximumRoundTime)
+		%LessButton.disabled = false
+	if (roundTime == maximumRoundTime): %MoreButton.disabled = true
+	update_timer()
+
+func _on_timer_less_button_pressed() -> void:
+	if (roundTime > 0):
+		roundTime = clampi(roundTime - 30, 0, maximumRoundTime)
+		%MoreButton.disabled = false
+	if (roundTime == 0): %LessButton.disabled = true
+	update_timer()
+
+func _on_minute_box_editing_toggled(toggled_on: bool) -> void:
+	if toggled_on: return
+	if timerInput.text.contains(":"):
+		var timerText = timerInput.text.split(":")
+		roundTime = int(timerText[0]) * 60 + int(timerText[1])
+	else:
+		roundTime = int(timerInput.text)
+	roundTime = clampi(roundTime, 0, maximumRoundTime)
+	update_timer()
+
+func update_timer() -> void:
+	@warning_ignore("integer_division")
+	timerInput.text = "%d:%02d" % [roundTime / 60, roundTime % 60]
+
+func _on_exit_button_pressed() -> void:
+	panel.visible = false
