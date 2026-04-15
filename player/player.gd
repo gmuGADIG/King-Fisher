b7250eecbfc1226ca60b20302e20a635e8c093bc
Michael Campbell <me@micycle.dev>
2026-04-15 18:36:38 -0400

    butterfly net works

--- player/player.gd
+++ player/player.gd
@@ -341,7 +341,7 @@ func pick_up_item(item: Item) -> void:
 	if held_item!=null: return
 	# TODO: Parent to player hand instead, with an offset for appropriate placement.
 	# Item origin is center/pickup area. Item hold point is offset in pos+rot.
-	item.reparent($Body, false)
+	item.reparent($DefaultPlayer, false)
 	held_item = item
 	held_item.player = self
 	held_item.is_held = true
