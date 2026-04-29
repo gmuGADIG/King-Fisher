663612cd6235827966e24cc5b82e3a4a2c5d09b8
KaratePikachu <KaidenKeyZ@gmail.com>
2026-04-29 17:11:56 -0400

    Clean up extraneous get_node

--- player/player_id.gd
+++ player/player_id.gd
@@ -1,4 +1,5 @@
 extends Label3D
 func _process(_delta: float) -> void:
-	if(get_node("/root/Multiplayer").player_list.has(get_multiplayer_authority())): #Do not try updating the dict with new players until they offically get recorded
-		text = str(get_node("/root/Multiplayer").player_list[get_multiplayer_authority()])
+	var multi_authority : int = get_multiplayer_authority()
+	if Multiplayer.player_list.has(multi_authority): #Do not try updating the dict with new players until they offically get recorded
+		text = str(Multiplayer.player_list[multi_authority])
