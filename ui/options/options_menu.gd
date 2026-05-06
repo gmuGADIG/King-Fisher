e8371afe919aebfa8211b3bef0b45519a5e7d804
KaratePikachu <KaidenKeyZ@gmail.com>
2026-05-06 13:22:54 -0400

    Music Mute debug option

--- ui/options/options_menu.gd
+++ ui/options/options_menu.gd
@@ -3,9 +3,18 @@ class_name Options
 
 static var mouse_sensitivity : float = 1
 static var aim_sesntivity : float = 1
-static var master_volume : float = 1
-static var sfx_volume : float = 1
-static var music_volume : float = 1
+static var master_volume : float = 1:
+	set(val):
+		master_volume = val
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), val)
+static var sfx_volume : float = 1:
+	set(val):
+		sfx_volume = val
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), val)
+static var music_volume : float = 1:
+	set(val):
+		music_volume = val
+		AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), val)
 
 @onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
 
@@ -22,14 +31,14 @@ func _process(_delta: float) -> void:
 		visible = not visible
 
 func _on_master_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), value)
+	master_volume = value
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("SFX"), value)
+	sfx_volume = value
 	
 	
 func _on_music_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Music"), value)
+	music_volume = value
 
 func _on_mouse_sensitivty_slider_value_changed(value: float) -> void:
 	mouse_sensitivity = value
