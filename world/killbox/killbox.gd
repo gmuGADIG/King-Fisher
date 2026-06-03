70599fe9bd5942c4e5b96eac939ff516afdb7108
KaratePikachu <KaidenKeyZ@gmail.com>
2026-06-03 17:32:06 -0400

    Increase killbox ragdoll time

--- world/killbox/killbox.gd
+++ world/killbox/killbox.gd
@@ -12,7 +12,7 @@ func _on_body_entered(body: Node3D) -> void:
 		if !body.force_respawn:
 			body.force_respawn = true
 			if body.is_ragdolled: return
-			body.ragdoll_phys.ragdoll(.05)
+			body.ragdoll_phys.ragdoll(2.0)
 			Debug.log("Killbox: " + body.name + " entered killbox, killing player.")
 	elif body.name == "Physical Bone Pelvis":
 		body = body.get_parent() # Get the Bones node from the pelvis bone
