accffa68ec594c7e5ff604cf152cc7183deb1efb
KaratePikachu <KaidenKeyZ@gmail.com>
2026-06-17 19:28:18 -0400

    Settings Control Panel

---
+++ ui/lobby_settings/settings_control_panel.gd
@@ -0,0 +1,17 @@
+extends Area3D
+
+@export var lobby_settings_menu : LobbySettings
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		if not multiplayer.is_server():
+			return
+		lobby_settings_menu.open()
+		
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is Player:
+		if not multiplayer.is_server():
+			return
+		lobby_settings_menu.close()
