427dcd6a8bab1fd09c09d689f502b01360d5d903
Shakeeb Uddin <115195258+Shakmak7866@users.noreply.github.com>
2026-07-09 11:56:07 -0400

    Pause Menu Animation Implemented (#726)
    * Pause Menu Animation Implemented
    
    Also, esc waits for animation to finish.
    
    * Fixed minor pause menu bugs
    
    Made spamming more stable, also toned down the animation please don't kill me shakeeb
    
    ---------
    
    Co-authored-by: KaratePikachu <KaidenKeyZ@gmail.com>

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@onready var anim : AnimationPlayer = $PauseFish/TextureRect/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,18 +12,24 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		if UIState.more_ui_blocked and UIState.ui_state != UIState.State.PAUSE:
 			return
-			
+		if $OptionsMenu.visible:
+			$OptionsMenu._on_exit_button_pressed()
+		
 		if not visible:
 			pause()
 		else:
 			unpause()
+		await anim.animation_finished
 
 func pause() -> void:
 	UIState.ui_state = UIState.State.PAUSE
 	visible = true
+	anim.play("drop_down")
 
 
 func unpause() -> void:
+	anim.play("pull_up",0.5)
+	await anim.animation_finished
 	UIState.ui_state = UIState.State.NONE
 	visible = false
 
