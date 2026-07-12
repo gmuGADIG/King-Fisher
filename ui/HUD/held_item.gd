a2f0e383af13f94c6c73af6e66fcbe9aafbfcd47
Michael Campbell <me@micycle.dev>
2026-07-11 21:19:29 -0400

    fix held item ui

--- ui/HUD/held_item.gd
+++ ui/HUD/held_item.gd
@@ -2,11 +2,22 @@ class_name HeldItemUI extends Control
 
 @onready var item_sprite: ItemSprite = %ItemSprite
 
+var item_to_string_table: Dictionary[Script, String] = {
+	BananaPeel: "BananaPeel",
+	FishingNet: "Butterfly Net",
+	GoldenWorm: "Golden Worm",
+	Helmet: "Helmet",
+	ZiplockBag: "Ziplock Bag",
+	RubberMallet: "Rubber Mallet",
+	GlueGrenade: "Glue Grenade",
+	Brick: "Brick"
+}
+
 # func _ready() -> void:
 # 	item_sprite.visible_item = "" # clear visible item
 
 func clear_item() -> void:
 	item_sprite.visible_item = ""
 
-func hold_item(item_name: String) -> void:
-	item_sprite.visible_item = item_name
+func hold_item(item: Item) -> void:
+	item_sprite.visible_item = item_to_string_table[item.get_script()]
